cee51daa462dfbfdee16946e40ade9d265987571
keeny <barry@craftstudios.co.nz>
2012-10-20 17:49:55 +1300

    spinning cube example

--- app/app.js
+++ app/app.js
@@ -1,64 +1,58 @@
 // Load the application once the DOM is ready, using `jQuery.ready`:
 $(function(){
 
-	// global object for our app
-	window.Hello3D = {
-		Models: {},
-		Controllers: {},
-		Views: {}
-	};
+// global namespace for our app
+window.Hello3D = {
+	Models: {},
+	Controllers: {},
+	Views: {}
+};
 
 // Use require js to load classes
 
-// The Application
-// ---------------
-
-// Our overall **AppView** is the top-level piece of UI.
+/**
+ * The app
+ * 
+ * @type {[type]}
+ */
 Hello3D.Views.App = Backbone.View.extend({
 
  	camera: null,
  	scene: null,
  	renderer: null,
- 	geometry: null,
- 	material: null,
- 	mesh: null,
+
+ 	cubeMesh: null,
 
     initialize: function() {
-    	// console.log('initialize');
-
     	this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
 	 	this.camera.position.z = 1000;
 
 	 	this.scene = new THREE.Scene();
 
-	 	this.geometry = new THREE.CubeGeometry( 200, 200, 200 );
-	 	this.material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
-
-	 	this.mesh = new THREE.Mesh( this.geometry, this.material );
-	 	this.scene.add( this.mesh );
-
 	 	this.renderer = new THREE.CanvasRenderer();
 	 	this.renderer.setSize( window.innerWidth, window.innerHeight );
-
-	 	document.body.appendChild( this.renderer.domElement );
     },
 
-    // Re-rendering the App just means refreshing the statistics -- the rest
-    // of the app doesn't change.
     render: function() {
-		console.log('render');
-
-		
+		document.body.appendChild( this.renderer.domElement );
 
 		return this;
     },
 
+    addProps: function() {
+	 	var cubeGeometry = new THREE.CubeGeometry( 200, 200, 200 );
+	 	var material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+
+	 	this.cubeMesh = new THREE.Mesh( cubeGeometry, material );
+	 	this.scene.add( this.cubeMesh );
+    },
+
     animate: function() {
 	    // note: three.js includes requestAnimationFrame shim
-	    requestAnimationFrame( this.animate );
+	    requestAnimationFrame( this.animate.bind(this) );
 
-	    this.mesh.rotation.x += 0.01;
-	    this.mesh.rotation.y += 0.02;
+	    this.cubeMesh.rotation.x += 0.01;
+	    this.cubeMesh.rotation.y += 0.02;
 
 	    this.renderer.render( this.scene, this.camera );
 	}
@@ -67,7 +61,8 @@ Hello3D.Views.App = Backbone.View.extend({
 
 // Finally, we kick things off by creating the **App**.
 var app = new Hello3D.Views.App;
-// App.render();
-// App.animate();
+app.render();
+app.addProps();
+app.animate();
 
 });
